0b5bea2d3ccd9c90ea644c5142fbeb4e389cb42d
Kushal Kumar <gaddamkushal321@gmail.com>
2026-01-12 15:20:17 +0530

    added AverageMark.java

---
+++ src/coreprogramming/programmingelements/level1/AverageMark.java
@@ -0,0 +1,11 @@
+package coreprogramming.programmingelements.level1;
+
+public class AverageMark {
+    public static void main(String[] args){
+        int maths = 94;
+        int phy = 95;
+        int chem = 96;
+
+        System.out.println((maths+phy+chem)/3);
+    }
+}
