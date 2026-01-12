afffb1605a3cd746eb84ff1973839ecddf3ebed6
Kushal Kumar <gaddamkushal321@gmail.com>
2026-01-12 15:15:58 +0530

    Added initital files

---
+++ src/coreprogramming/programmingelements/level1/KmToMiles.java
@@ -0,0 +1,12 @@
+package coreprogramming.programmingelements.level1;
+
+public class KmToMiles {
+    public static void main(String[] args) {
+
+        double kilometers = 10.8;
+        double miles = kilometers * 1.6;
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
+
