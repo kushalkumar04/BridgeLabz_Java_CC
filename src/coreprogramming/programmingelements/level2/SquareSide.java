f202e114658a8e4798caf93d5528f6ee06a233fd
Kushal Kumar <gaddamkushal321@gmail.com>
2026-01-13 11:17:24 +0530

    Added the rem files

---
+++ src/coreprogramming/programmingelements/level2/SquareSide.java
@@ -0,0 +1,22 @@
+package coreprogramming.programmingelements.level2;
+
+
+
+import java.util.Scanner;
+
+public class SquareSide {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double perimeter = input.nextDouble();
+        double side = perimeter / 4;
+
+        System.out.println(
+                "The length of the side is " + side +
+                        " whose perimeter is " + perimeter
+        );
+
+        input.close();
+    }
+}
