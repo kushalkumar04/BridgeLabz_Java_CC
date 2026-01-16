d7e1abf1a987f37d3eb63671e06274e8b6e743b7
Kushal Kumar <157942925+kushalkumar04@users.noreply.github.com>
2026-01-16 15:46:17 +0530

    Update RocketLaunchCountdown.java

--- src/coreprogramming/controlflows/level1/RocketLaunchCountdown.java
+++ src/coreprogramming/controlflows/level1/RocketLaunchCountdown.java
@@ -1,14 +1,29 @@
 package coreprogramming.controlflows.level1;
+
 import java.util.Scanner;
+
+/*
+ * This class simulates a rocket launch countdown.
+ * It takes a starting number from the user and counts down
+ * to 1 using a while loop, printing each value.
+ */
 public class RocketLaunchCountdown {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input from the user
         Scanner input = new Scanner(System.in);
 
+        // Read the starting value for the countdown
         int counter = input.nextInt();
 
-        while (counter >= 1){
+        // Loop until the counter reaches 1
+        while (counter >= 1) {
+
+            // Print the current countdown number
             System.out.println(counter);
 
+            // Decrease the counter by 1 in each iteration
             counter--;
         }
     }
