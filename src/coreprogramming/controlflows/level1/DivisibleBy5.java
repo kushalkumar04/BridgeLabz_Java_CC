5ef5d40b8d6c71b40c405b6eb1d32755224a68b4
Kushal Kumar <157942925+kushalkumar04@users.noreply.github.com>
2026-01-16 15:44:02 +0530

    Update DivisibleBy5.java

--- src/coreprogramming/controlflows/level1/DivisibleBy5.java
+++ src/coreprogramming/controlflows/level1/DivisibleBy5.java
@@ -1,15 +1,29 @@
 package coreprogramming.controlflows.level1;
+
 import java.util.Scanner;
+
+/*
+ * This class checks whether a given integer is divisible by 5.
+ * It takes input from the user and prints the result accordingly.
+ */
 public class DivisibleBy5 {
 
     public static void main(String[] args) {
+        
+        // Create Scanner object to take input from the user
         Scanner input = new Scanner(System.in);
+        
+        // Read an integer value from the user
         int number = input.nextInt();
-        if (number % 5 == 0){
+        
+        // Check if the number is divisible by 5
+        if (number % 5 == 0) {
+            // If remainder is 0, the number is divisible by 5
             System.out.println("It is divisible");
-        }
-        else{
-            System.out.println("NOt divisible");
+        } 
+        else {
+            // If remainder is not 0, the number is not divisible by 5
+            System.out.println("Not divisible");
         }
     }
 }
